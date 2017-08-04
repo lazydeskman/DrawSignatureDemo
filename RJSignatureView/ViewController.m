5a8c244e38f56e69ef7b8b5ce736e08dc7ada016
WangAn <wangan@appledeMacBook-Pro.local>
2017-08-04 13:11:18 +0800

    modified:   RJSignatureView.xcodeproj/project.xcworkspace/xcuserdata/wangan.xcuserdatad/UserInterfaceState.xcuserstate 	modified:   RJSignatureView/ViewController.m

--- RJSignatureView/ViewController.m
+++ RJSignatureView/ViewController.m
@@ -21,45 +21,43 @@
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = UIColor.whiteColor;
     self.navigationController.navigationBar.barTintColor = UIColor.blackColor;
-//    UIBarButtonItem * red = [[UIBarButtonItem alloc]initWithTitle:@"红色" style:UIBarButtonItemStylePlain target:self action:@selector(rightAction:)];
-//    UIBarButtonItem * back = [[UIBarButtonItem alloc]initWithTitle:@"后退" style:UIBarButtonItemStylePlain target:self action:@selector(saveAction:)];
-//    self.navigationItem.rightBarButtonItems = @[red,back];
-//    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"橙色" style:UIBarButtonItemStylePlain target:self action:@selector(leftAction:)];
-    UIBarButtonItem * redLine = [[UIBarButtonItem alloc]initWithTitle:@"红色" style:UIBarButtonItemStylePlain target:self action:@selector(redLine:)];
-    UIBarButtonItem * greenLine = [[UIBarButtonItem alloc]initWithTitle:@"绿色" style:UIBarButtonItemStylePlain target:self action:@selector(greenLine:)];
-    UIBarButtonItem * back = [[UIBarButtonItem alloc]initWithTitle:@"后退" style:UIBarButtonItemStylePlain target:self action:@selector(backword:)];
-    UIBarButtonItem * forword = [[UIBarButtonItem alloc]initWithTitle:@"前进" style:UIBarButtonItemStylePlain target:self action:@selector(forword:)];
-    UIBarButtonItem * estart = [[UIBarButtonItem alloc]initWithTitle:@"橡皮开始" style:UIBarButtonItemStylePlain target:self action:@selector(eraseStart:)];
-    UIBarButtonItem * eend = [[UIBarButtonItem alloc]initWithTitle:@"橡皮结束" style:UIBarButtonItemStylePlain target:self action:@selector(eraseEnd:)];
-    self.navigationItem.leftBarButtonItems = @[redLine,greenLine,estart];
-    self.navigationItem.rightBarButtonItems = @[back,forword,eend];
+    UIBarButtonItem * line = [[UIBarButtonItem alloc]initWithTitle:@"线条" style:UIBarButtonItemStylePlain target:self action:@selector(randomLineWidth:)];
+    UIBarButtonItem * color = [[UIBarButtonItem alloc]initWithTitle:@"颜色" style:UIBarButtonItemStylePlain target:self action:@selector(randomLineColor:)];
+    UIBarButtonItem * back = [[UIBarButtonItem alloc]initWithTitle:@"后退" style:UIBarButtonItemStylePlain target:self action:@selector(backwordInput:)];
+    UIBarButtonItem * forword = [[UIBarButtonItem alloc]initWithTitle:@"前进" style:UIBarButtonItemStylePlain target:self action:@selector(forwordInput:)];
+    UIBarButtonItem * eraS = [[UIBarButtonItem alloc]initWithTitle:@"擦除" style:UIBarButtonItemStylePlain target:self action:@selector(eraseStart:)];
+    UIBarButtonItem * eraE = [[UIBarButtonItem alloc]initWithTitle:@"手写" style:UIBarButtonItemStylePlain target:self action:@selector(eraseEnd:)];
+    self.navigationItem.leftBarButtonItems = @[line,color,back];
+    self.navigationItem.rightBarButtonItems = @[forword,eraS,eraE];
     sigView = [[LRSignView alloc]initWithFrame:self.view.bounds];
     [self.view addSubview:sigView];
 }
 
-- (void)redLine:(id)sender {
-    sigView.lineColor = UIColor.redColor;
+- (void)randomLineColor:(id)sender {
+    UIColor * color = [UIColor colorWithRed:(arc4random()%256)/255.f green:(arc4random()%256)/255.f blue:(arc4random()%256)/255.f alpha:(arc4random()%256)/255.f];
+    sigView.lineColor = color;
 }
 
-- (void)greenLine:(id)sender {
-    sigView.lineColor = UIColor.greenColor;
+- (void)randomLineWidth:(id)sender {
+    CGFloat width = (arc4random()%10)/1000.f;
+    sigView.lineWidth = width;
 }
 
-- (void)backword:(id)sender {
+- (void)backwordInput:(id)sender {
     [sigView backword];
 }
 
-- (void)forword:(id)sender {
+- (void)forwordInput:(id)sender {
     [sigView forword];
 }
 
 - (void)eraseStart:(id)sender {
     [sigView eraserBegin];
 }
+
 - (void)eraseEnd:(id)sender {
     [sigView eraserEnd];
 }
-
 
 
 @end
